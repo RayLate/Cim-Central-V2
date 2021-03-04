bc74b685a3385233d44b72cf0e6c396fea8900e8
Ray <72261323+RayLate@users.noreply.github.com>
2021-03-04 16:24:33 +0800

    Add files via upload

---
+++ Demo/app.js
@@ -0,0 +1,8 @@
+// document.getElementById('deploy').addEventListener('click', newDeployment);
+
+
+// const selectsection = document.getElementById('selector-main');
+
+// function newDeployment() {
+//     selectsection.innerHTML = '<object type="text/html" data="deploy.html" ></object>';
+// }
