a21a2eab5866d8fe2e353058b250ca2fa1ec4ec8
Ray <72261323+RayLate@users.noreply.github.com>
2021-03-23 22:30:46 +0800

    Update tests.py

--- app/inventory/tests.py
+++ app/inventory/tests.py
@@ -1,3 +1,5 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
 # Create your tests here.
